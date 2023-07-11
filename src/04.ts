5306fef35a4886dddf79049e442286a6ce20cffe
husnain <husnain2info@gmail.com>
2023-07-11 12:45:56 +0500

    quotes problem is resolved

---
+++ src/04.ts
@@ -0,0 +1,7 @@
+/**
+ * Famous Quote 2: Repeat Exercise 4, but this time store the famous person’s name in a variable called famous_person. Then compose your message and store it in a new variable called message. Print your message.
+ */
+let famouse_person: string = "Ralph Waldo Emerson";
+let message: string = `${famouse_person} once said "Every artist was first an amateur."`;
+
+console.log(message);

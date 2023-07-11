abe9be748f4c79e68be7ca3f545bdbaa324ea798
husnain <husnain2info@gmail.com>
2023-07-11 13:36:39 +0500

    problem 10: comments added

--- src/07.ts
+++ src/07.ts
@@ -1,6 +1,9 @@
+/**
+ * Name: Husnain Ahmad
+ * Date: 11/07/2023
+ */
 
-
-console.log(5 + 3);
-console.log(10 - 2);
-console.log(4 * 2);
-console.log(16 / 2);
+console.log(5 + 3); // doing addition in this statement
+console.log(10 - 2); // doing subtraction in this statement
+console.log(4 * 2); // doing multiplication in this statement
+console.log(16 / 2); // doing division in this statement
--- src/08.ts
+++ src/08.ts
@@ -1,3 +1,3 @@
 //Favorite Number: Store your favorite number in a variable. Then, using that variable, create a message that reveals your favorite number. Print that message.
 let favorite_number_08: number = 7;
-console.log(`My favorite number is ${favorite_number_08}`);
+console.log(`My favorite number is ${favorite_number_08}`); // printing favorite number into string using console.log

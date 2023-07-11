36373529863e4bdd2b3ea7c5432f7bf7c0bf220d
husnain <husnain2info@gmail.com>
2023-07-11 12:39:48 +0500

    problem 03 of favorite quote added

---
+++ src/03.ts
@@ -0,0 +1,7 @@
+/**
+ * Problem: Famous Quote: Find a quote from a famous person you admire. Print the quote and the name of its author. Your output should look something like the following, including the quotation marks:
+ * Albert Einstein once said, “A person who never made a mistake never tried anything new.”
+ */
+
+let quote: string = `Moulana Rumi once said, "Raise your words, not voice. It is rain that grows flowers, not thunder."`;
+console.log(quote)

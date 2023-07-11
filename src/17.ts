d8c345ed5b5af35526754527bd0e9336fcf59d2d
husnain <husnain2info@gmail.com>
2023-07-11 17:41:08 +0500

    Problem 17: ony 2 people are invited

---
+++ src/17.ts
@@ -0,0 +1,22 @@
+let guest_list_17: string[] = ["Arsalan", "Faizan", "Riaz", "Murtaza"];
+
+let guest_List_can_not_come_17: string[] = [guest_list_17[1]];
+// Faizan is not coming so let's invite Husnain
+guest_list_17.splice(1, 1, "Husnain");
+
+guest_list_17.unshift("Peter"); // adding a new guest at the start of the list
+
+let middle_of_array_17: number = Math.floor(guest_list_17.length / 2);
+guest_list_17.splice(middle_of_array_17, 0, "John"); // adding guest in the middle of the list
+
+guest_list_17.push("Tony"); // adding a new guest at the end of the list
+
+
+console.log("I appologize! My dinner table won't arrive in time for dinner.");
+console.log("I can only invite two people for dinner.");
+
+guest_list_17.splice(2, guest_list_17.length - 2);
+
+guest_list_17.forEach((item) => {
+    console.log(`Hello ${item} ! You are still invited to dinner.`);
+});

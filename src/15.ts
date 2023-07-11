a5a26a66ce18d4f710959d02936c8d90d5cc4225
husnain <husnain2info@gmail.com>
2023-07-11 14:01:40 +0500

    printed the list of guests who could not come

--- src/15.ts
+++ src/15.ts
@@ -4,9 +4,14 @@
 
 let guest_list_15: string[] = ["Arsalan", "Faizan", "Riaz", "Murtaza"];
 
+let guest_List_can_not_come: string[] = [guest_list_15[1]];
 // Faizan is not coming so let's invite Husnain
 guest_list_15.splice(1, 1, "Husnain");
 
 guest_list_15.forEach((item) => {
     console.log(`Hello ${item} ! You are invited to dinner.`);
 });
+
+guest_List_can_not_come.forEach((item) => {
+    console.log(`Sorry ${item} ! You were invited but cannot make it.`);
+})

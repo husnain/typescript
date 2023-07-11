6813ab06415f3d3ffbc9b2149830379075863aca
husnain <husnain2info@gmail.com>
2023-07-11 13:19:27 +0500

    problem 06 and problem 07 has been resolved

---
+++ src/06.ts
@@ -0,0 +1,9 @@
+//Write addition, subtraction, multiplication, and division operations that each result in the number 8
+let addition_06 = 5 + 3;
+let subtraction_06 = 10 - 2;
+let multiplication_06 = 4 * 2;
+let division_06 = 16 / 2;
+console.log(addition_06);
+console.log(subtraction_06);
+console.log(multiplication_06);
+console.log(division_06);

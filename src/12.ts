e9cef7503d5dac53bf867ceb801c4fdd56404a5f
husnain <husnain2info@gmail.com>
2023-07-11 13:42:52 +0500

    Problem 12: Greetings

---
+++ src/12.ts
@@ -0,0 +1,9 @@
+/**
+ * Problem: Names: Store the names of a few of your friends in a array called names. Print each person’s name by accessing each element in the list, one at a time.
+ */
+
+let names_12: string[] = ["Husnain", "Arsalan", "Faizan", "Riaz", "Murtaza"];
+
+for (let i = 0; i < names_12.length; i++) {
+    console.log(`Hello ${names_12[i]} ! How are you? `);
+}

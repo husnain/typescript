ff6408ad404b90d2ae9b0ed3651fcf3911bb4de6
husnain <husnain2info@gmail.com>
2023-07-11 13:53:17 +0500

    Problem 14: guest list invitation

---
+++ src/14.ts
@@ -0,0 +1,9 @@
+/**
+ * Guest List: If you could invite anyone, living or deceased, to dinner, who would you invite? Make a list that includes at least three people you’d like to invite to dinner. Then use your list to print a message to each person, inviting them to dinner.
+ */
+
+let guest_list_14: string[] = ["Arsalan", "Faizan", "Riaz", "Murtaza"];
+
+guest_list_14.forEach((item) => {
+    console.log(`Hello ${item} ! You are invited to dinner.`);
+});

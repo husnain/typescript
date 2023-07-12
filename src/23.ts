296682341c384b4d25ee8269db5a4bba322dec7a
husnain <husnain2info@gmail.com>
2023-07-12 13:12:30 +0500

    conditional tests

---
+++ src/23.ts
@@ -0,0 +1,6 @@
+let phone: string = "IPhone";
+
+console.log(phone == "IPhone" ? "I have an IPhone" : "I have an Android");
+console.log(phone == "Android" ? "I have an Android" : "I have an IPhone")
+console.log(phone == "IPhone");
+console.log(phone == "android");

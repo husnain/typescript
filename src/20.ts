3bafc867e362c4ced0e487aff0c2df057e029fe5
husnain <husnain2info@gmail.com>
2023-07-12 12:48:20 +0500

    problem 20: making rivers list

---
+++ src/20.ts
@@ -0,0 +1,11 @@
+/**
+ * Think of something you could store in a array. For example, you could make a list of mountains, rivers, countries, cities, languages, or anything else you’d like. Write a program that creates a list containing these items.
+ */
+let rivers: string[] = [];
+rivers.push("Nile");
+rivers.push("Amazon");
+rivers.push("Yangtze");
+rivers.push("Mississippi");
+rivers.push("Yenisei");
+
+console.log(rivers);

98a11f98a1ba9bc3ba4dff9c18bcd5e9491c067a
husnain <husnain2info@gmail.com>
2023-07-12 13:45:40 +0500

    Problem 23 conditional tests

--- src/23.ts
+++ src/23.ts
@@ -2,5 +2,13 @@ let phone: string = "IPhone";
 
 console.log(phone == "IPhone" ? "I have an IPhone" : "I have an Android");
 console.log(phone == "Android" ? "I have an Android" : "I have an IPhone")
-console.log(phone == "IPhone");
-console.log(phone == "android");
+console.log(phone == "android"); // false
+console.log(phone == "Android"); // false
+console.log(phone == "iphone"); // false
+console.log(phone == "Apple"); // false
+console.log(phone != "IPhone"); // false
+console.log(phone == "IPhone"); // true
+console.log(phone == "IPhone" || phone == "Android"); // true
+console.log(phone == "IPhone" || phone == "Android" || phone == "iphone" || phone == "android"); // true
+console.log(phone != "Android"); // true
+console.log(phone != "iphone"); // true
